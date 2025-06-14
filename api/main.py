c182dd154bc3bb58889d95feed99bc54696919e6
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2025-06-14 19:43:16 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from chainlit.utils import mount_chainlit
+#from chainlit.utils import mount_chainlit
 
 
 
@@ -17,4 +17,4 @@ def chainie():
 def chainies():  
     return {"message": "Hello World  from /api/api2"}
 
-mount_chainlit(app=app, target="my_cl_app.py", path="/chainlit")
+#mount_chainlit(app=app, target="my_cl_app.py", path="/chainlit")
