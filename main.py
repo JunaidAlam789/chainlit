258e0a3e80aed333ce2b88ceb4b5b9781ee1d0f5
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2025-06-14 17:45:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 #from chainlit.utils import mount_chainlit
 
 
-app2 = FastAPI(root_path="/")
+app = FastAPI(root_path="/")
               
-@app2.get("/app")
+@app.get("/app")
 def read_main():
     return {"message": "Hello World from main app"}
 
