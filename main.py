cff27c8ff2b45f4eac4a37115d65c988633effd8
Muhammad Junaid Alam <89206859+JunaidAlam789@users.noreply.github.com>
2025-06-14 17:44:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,6 @@ from fastapi import FastAPI
 #from chainlit.utils import mount_chainlit
 
 
-
-# import functools
-# import importlib
-# import inspect
-# import os
-# from asyncio import CancelledError
-# from typing import Callable
-
-# import click
-# from fastapi import FastAPI, Request
-# from fastapi.responses import JSONResponse
-# from packaging import version
-# from starlette.middleware.base import BaseHTTPMiddleware
-
-#from chainlit.auth import ensure_jwt_secret
-# from chainlit.context import context
-# from chainlit.logger import logger
-# from chainlit.message import ErrorMessage
-
-
-
 app2 = FastAPI(root_path="/")
               
 @app2.get("/app")
